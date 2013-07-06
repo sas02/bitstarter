8723b0ff59361bb32dd61ea12f01011a05960a6c
santosh-sas <santoshsas02@gmail.com>
2013-07-06 23:27:12 +0530

    reading from disk

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var buf = new Buffer(256);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Welcome santosh how are you !');
+  fs.readFile('index.html','utf8',function(err,data){
+  	if (err) throw err;
+	response.send(data);
 });
+  
+}) ;
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
